5cf5f92c0947adc686e7b5491e7a643268ee38a3
nvko <nvko@frsupc088>
2020-02-24 09:44:10 +0100

    banner

--- powa-comparator.py
+++ powa-comparator.py
@@ -1,12 +1,31 @@
 #!/usr/bin/env python
+#!/usr/bin/env python
 # author: Julien ESCOFFIER
 # email: julien.escoffier@protonmail.com
 #
-# Todo: 
+# Todo:
 # - add installation instructions(README.md)
 # - add an addition license plate website (for informations crossing & verification)
 # - add technical stats of vehicule
 # - add comparator (with a saved license plate) (0-60) (400m) (1/4 mile) ...
+
+
+def banner():
+    print(" _________")
+    print("\_   ___ \  ____   _____ ______")
+    print("/    \  \/ /  _ \ /     \\____ \ ")
+    print("\     \___(  <_> )  Y Y  \  |_> >")
+    print("\______  /\____/|__|_|  /   __/")
+    print("     ________        \/|__|")
+    print("     \______ \ _____")
+    print("      |    |  \\__  \ ")
+    print("      |    `   \/ __ \_")
+    print("_______________  (____  /")
+    print("\______   \______  _  _______")
+    print("|     ___/  _ \ \/ \/ /\__  \ ")
+    print("|    |  (  <_> )     /  / __ \_")
+    print("|____|   \____/ \/\_/  (____  /")
+    print("                           \/  ")
 
 
 import time
@@ -15,6 +34,7 @@ import urllib
 import bs4
 from urllib import request
 from os import system
+
 
 def plaque_format(plaque_raw):
     plaque = ''
@@ -41,30 +61,33 @@ def yak_parse(url_yak):
 
 
 # Test longueur de la plaque 3min(1A1) 10max(9999ZZZ999)
+banner()
 plaque_raw = input("Plaque: ")
 while len(plaque_raw) <=3:
     print("La plaque est trop courte.")
     time.sleep(1)
     system('clear')
+    banner()
     plaque_raw = input("\nPlaque: ")
 
 while len(plaque_raw) >= 11:
     print("La plaque est trop longue.")
     time.sleep(1)
     system('clear')
+    banner()
     plaque_raw = input("\nPlaque: ")
 
-print(plaque_format(plaque_raw))
+#print(plaque_format(plaque_raw))
 
 # YAKAROULER
 print("YAKAROULER: " + yak_parse("https://www.yakarouler.com/car_search/immat?immat=" + plaque_format(plaque_raw) + "&name=undefined&redirect=true"))
 
 # PIECESAUTO.COM
 
-# OSCARO.COM
-url_string_osc = 'https://www.oscaro.com/Catalog/SearchEngine/GetPlateSearchResult'
-data = "plateValue=" + plaque_format(plaque_raw) + "&genartId=null"
-data = data.encode('utf-8')
-req_osc = urllib.request.Request(url=url_string_osc, data=data, method='POST', headers={'User-Agent': 'Mozilla/5.0'})
-response_osc = urllib.request.urlopen(req_osc).read()
-print("\n\nOSCARO\n" + str(response_osc))
+# OSCARO.COM (QA in progress in scratch-oscaro.py)
+#url_string_osc = 'https://www.oscaro.com/Catalog/SearchEngine/GetPlateSearchResult'
+#data = "plateValue=" + plaque_format(plaque_raw) + "&genartId=null"
+#data = data.encode('utf-8')
+#req_osc = urllib.request.Request(url=url_string_osc, data=data, method='POST', headers={'User-Agent': 'Mozilla/5.0'})
+#response_osc = urllib.request.urlopen(req_osc).read()
+#print("\n\nOSCARO\n" + str(response_osc))
